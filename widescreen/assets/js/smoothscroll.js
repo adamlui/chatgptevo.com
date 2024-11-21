54cbc26fba86599a12c6d5e805dcd970d2c7bad5
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-21 13:51:51 -0800

    Added @stylistic/eslint-plugin-js as dev dependency to lint trailing whitespace

--- widescreen/assets/js/smoothscroll.js
+++ widescreen/assets/js/smoothscroll.js
@@ -6,15 +6,15 @@ function smoothScroll(target, speed, smooth) {
 
     // Init target
     if (target === document)
-        target = (document.scrollingElement 
-              || document.documentElement 
-              || document.body.parentNode 
+        target = (document.scrollingElement
+              || document.documentElement
+              || document.body.parentNode
               || document.body); // cross browser support for document scrolling
 
     // Init variables
     let moving = false, pos = target.scrollTop;
-    const frame = target === document.body && document.documentElement 
-                      ? document.documentElement 
+    const frame = target === document.body && document.documentElement
+                      ? document.documentElement
                       : target; // safari
     // Add listeners
     target.addEventListener('mousewheel', scrolled, { passive: false });
@@ -38,8 +38,8 @@ function smoothScroll(target, speed, smooth) {
     }
 
     function update() {
-        moving = true;    
-        const delta = (pos - target.scrollTop) / smooth;    
+        moving = true;
+        const delta = (pos - target.scrollTop) / smooth;
         target.scrollTop += delta;
         if (Math.abs(delta) > 0.5) requestFrame(update);
         else moving = false;
