2ad758a7bb93e38755a580628c89f6f62431a2c7
Adam Lui <adam@kudoai.com>
2024-11-26 22:31:45 -0800

    Added `js-styles/no-extra-semi` to JS config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default [
             'js-styles/max-len': ['error', { 'code': 120, // limit lines to 120 chars except if containing...
                 'ignoreComments': true, 'ignoreStrings': true, // ...trailing/own-line comments, quoted strings...
                 'ignoreTemplateLiterals': true, 'ignoreRegExpLiterals': true }], // ...or template/regex literals
+            'js-styles/no-extra-semi': 'error', // disallow unnecessary semicolons
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
