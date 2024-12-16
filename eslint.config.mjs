fd7ef633e49ccb8218a026bb1dbaf70c26a1f14d
adamlui@protonmail.com <adamlui@protonmail.com>
2024-12-16 00:22:42 -0800

    Implemented eslint-plugin-import as dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import stylisticJS from '@stylistic/eslint-plugin-js'
+import importPlugin from 'eslint-plugin-import'
 import json from '@eslint/json'
 import regexp from 'eslint-plugin-regexp'
+import stylisticJS from '@stylistic/eslint-plugin-js'
 import yml from 'eslint-plugin-yml'
 
 export default [
@@ -10,9 +11,11 @@ export default [
     {
         files: ['**/*.js', '**/*.mjs'],
         languageOptions: { ecmaVersion: 'latest', sourceType: 'script', globals: { ...globals.browser }},
-        plugins: { regexp, 'js-styles': stylisticJS },
+        plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
         rules: {
-            ...js.configs.recommended.rules, ...regexp.configs['flat/recommended'].rules,
+            ...js.configs.recommended.rules,
+            ...importPlugin.flatConfigs.recommended.rules,
+            ...regexp.configs['flat/recommended'].rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
             'js-styles/no-trailing-spaces': 'error', // ...except at ends of lines
             'js-styles/max-len': ['error', { 'code': 120, // limit lines to 120 chars except if containing...
